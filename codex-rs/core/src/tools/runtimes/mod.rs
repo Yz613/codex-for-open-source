openai/codex

fix: Refresh proxy env after shell snapshots

codex-rs/core/src/tools/runtimes/mod.rs
@@ -8,6 +8,8 @@ use crate::exec_env::CODEX_THREAD_ID_ENV_VAR;
 use crate::path_utils;
 use crate::shell::Shell;
 use crate::tools::sandboxing::ToolError;
+use codex_network_proxy::ALLOW_LOCAL_BINDING_ENV_KEY;
+use codex_network_proxy::MANAGED_PROXY_ENV_KEYS;
 use codex_protocol::models::PermissionProfile;
 use codex_sandboxing::SandboxCommand;
 use codex_utils_absolute_path::AbsolutePathBuf;
@@ -110,6 +112,9 @@ pub(crate) fn maybe_wrap_shell_lc_with_snapshot(
         override_env.insert(CODEX_THREAD_ID_ENV_VAR.to_string(), thread_id.clone());
     }
     let (override_captures, override_exports) = build_override_exports(&override_env);
+    let (proxy_captures, proxy_exports) = build_managed_proxy_env_exports();
+    let override_captures = join_shell_blocks([override_captures, proxy_captures]);
+    let override_exports = join_shell_blocks([override_exports, proxy_exports]);
     let rewritten_script = if override_exports.is_empty() {
         format!(
             "if . '{snapshot_path}' >/dev/null 2>&1; then :; fi\n\nexec '{original_shell}' -c '{original_script}'{trailing_args}"
@@ -126,10 +131,35 @@ pub(crate) fn maybe_wrap_shell_lc_with_snapshot(
 fn build_override_exports(explicit_env_overrides: &HashMap<String, String>) -> (String, String) {
     let mut keys = explicit_env_overrides
         .keys()
+        .map(String::as_str)
         .filter(|key| is_valid_shell_variable_name(key))
         .collect::<Vec<_>>();
     keys.sort_unstable();
 
+    build_override_exports_for_keys("__CODEX_SNAPSHOT_OVERRIDE", &keys)
+}
+
+fn build_managed_proxy_env_exports() -> (String, String) {
+    let mut keys = MANAGED_PROXY_ENV_KEYS
+        .iter()
+        .copied()
+        .filter(|key| is_valid_shell_variable_name(key))
+        .collect::<Vec<_>>();
+    keys.sort_unstable();
+    keys.dedup();
+
+    let (captures, restores) =
+        build_override_exports_for_keys("__CODEX_SNAPSHOT_PROXY_OVERRIDE", &keys);
+    let key = ALLOW_LOCAL_BINDING_ENV_KEY;
+    (
+        format!(
+            "__CODEX_SNAPSHOT_PROXY_ENV_SET=\"${{{key}+x}}\"\nif [ -n \"$__CODEX_SNAPSHOT_PROXY_ENV_SET\" ]; then\n{captures}\nfi"
+        ),
+        format!("if [ -n \"$__CODEX_SNAPSHOT_PROXY_ENV_SET\" ]; then\n{restores}\nfi"),
+    )
+}
+
+fn build_override_exports_for_keys(variable_prefix: &str, keys: &[&str]) -> (String, String) {
     if keys.is_empty() {
         return (String::new(), String::new());
     }
@@ -138,9 +168,9 @@ fn build_override_exports(explicit_env_overrides: &HashMap<String, String>) -> (
         .iter()
         .enumerate()
         .map(|(idx, key)| {
-            format!(
-                "__CODEX_SNAPSHOT_OVERRIDE_SET_{idx}=\"${{{key}+x}}\"\n__CODEX_SNAPSHOT_OVERRIDE_{idx}=\"${{{key}-}}\""
-            )
+            let set_var = format!("{variable_prefix}_SET_{idx}");
+            let value_var = format!("{variable_prefix}_{idx}");
+            format!("{set_var}=\"${{{key}+x}}\"\n{value_var}=\"${{{key}-}}\"")
         })
         .collect::<Vec<_>>()
         .join("\n");
@@ -148,14 +178,24 @@ fn build_override_exports(explicit_env_overrides: &HashMap<String, String>) -> (
         .iter()
         .enumerate()
         .map(|(idx, key)| {
+            let set_var = format!("{variable_prefix}_SET_{idx}");
+            let value_var = format!("{variable_prefix}_{idx}");
             format!(
-                "if [ -n \"${{__CODEX_SNAPSHOT_OVERRIDE_SET_{idx}}}\" ]; then export {key}=\"${{__CODEX_SNAPSHOT_OVERRIDE_{idx}}}\"; else unset {key}; fi"
+                "if [ -n \"${{{set_var}}}\" ]; then export {key}=\"${{{value_var}}}\"; else unset {key}; fi"
             )
         })
         .collect::<Vec<_>>()
         .join("\n");
 
     (captures, restores)
+}
+
+fn join_shell_blocks(blocks: impl IntoIterator<Item = String>) -> String {
+    blocks
+        .into_iter()
+        .filter(|block| !block.is_empty())
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
 fn is_valid_shell_variable_name(name: &str) -> bool {

codex-rs/network-proxy/src/lib.rs
@@ -34,6 +34,7 @@ pub use proxy::ALL_PROXY_ENV_KEYS;
 pub use proxy::ALLOW_LOCAL_BINDING_ENV_KEY;
 pub use proxy::Args;
 pub use proxy::DEFAULT_NO_PROXY_VALUE;
+pub use proxy::MANAGED_PROXY_ENV_KEYS;
 pub use proxy::NO_PROXY_ENV_KEYS;
 pub use proxy::NetworkProxy;
 pub use proxy::NetworkProxyBuilder;

codex-rs/network-proxy/src/proxy.rs
@@ -364,6 +364,44 @@ pub const PROXY_URL_ENV_KEYS: &[&str] = &[
 
 pub const ALL_PROXY_ENV_KEYS: &[&str] = &["ALL_PROXY", "all_proxy"];
 pub const ALLOW_LOCAL_BINDING_ENV_KEY: &str = "CODEX_NETWORK_ALLOW_LOCAL_BINDING";
+const ELECTRON_GET_USE_PROXY_ENV_KEY: &str = "ELECTRON_GET_USE_PROXY";
+const GIT_SSH_COMMAND_ENV_KEY: &str = "GIT_SSH_COMMAND";
+pub const MANAGED_PROXY_ENV_KEYS: &[&str] = &[
+    ALLOW_LOCAL_BINDING_ENV_KEY,
+    ELECTRON_GET_USE_PROXY_ENV_KEY,
+    GIT_SSH_COMMAND_ENV_KEY,
+    "HTTP_PROXY",
+    "HTTPS_PROXY",
+    "http_proxy",
+    "https_proxy",
+    "YARN_HTTP_PROXY",
+    "YARN_HTTPS_PROXY",
+    "npm_config_http_proxy",
+    "npm_config_https_proxy",
+    "npm_config_proxy",
+    "NPM_CONFIG_HTTP_PROXY",
+    "NPM_CONFIG_HTTPS_PROXY",
+    "NPM_CONFIG_PROXY",
+    "BUNDLE_HTTP_PROXY",
+    "BUNDLE_HTTPS_PROXY",
+    "PIP_PROXY",
+    "DOCKER_HTTP_PROXY",
+    "DOCKER_HTTPS_PROXY",
+    "WS_PROXY",
+    "WSS_PROXY",
+    "ws_proxy",
+    "wss_proxy",
+    "NO_PROXY",
+    "no_proxy",
+    "npm_config_noproxy",
+    "NPM_CONFIG_NOPROXY",
+    "YARN_NO_PROXY",
+    "BUNDLE_NO_PROXY",
+    "ALL_PROXY",
+    "all_proxy",
+    "FTP_PROXY",
+    "ftp_proxy",
+];
 
 const FTP_PROXY_ENV_KEYS: &[&str] = &["FTP_PROXY", "ftp_proxy"];
 const WEBSOCKET_PROXY_ENV_KEYS: &[&str] = &["WS_PROXY", "WSS_PROXY", "ws_proxy", "wss_proxy"];
@@ -382,6 +420,11 @@ pub const DEFAULT_NO_PROXY_VALUE: &str = concat!(
     "*.local,.local,",
     "169.254.0.0/16,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
 );
+
+#[cfg(target_os = "macos")]
+const MANAGED_GIT_SSH_COMMAND_PREFIX: &str = "ssh -o ProxyCommand='nc -X 5 -x ";
+#[cfg(target_os = "macos")]
+const MANAGED_GIT_SSH_COMMAND_SUFFIX: &str = " %h %p'";
 
 pub fn proxy_url_env_value<'a>(
     env: &'a HashMap<String, String>,
@@ -404,6 +447,17 @@ fn set_env_keys(env: &mut HashMap<String, String>, keys: &[&str], value: &str) {
     for key in keys {
         env.insert((*key).to_string(), value.to_string());
     }
+}
+
+#[cfg(target_os = "macos")]
+fn managed_git_ssh_command(socks_addr: SocketAddr) -> String {
+    format!("{MANAGED_GIT_SSH_COMMAND_PREFIX}{socks_addr}{MANAGED_GIT_SSH_COMMAND_SUFFIX}")
+}
+
+#[cfg(target_os = "macos")]
+fn is_managed_git_ssh_command(command: &str) -> bool {
+    command.starts_with(MANAGED_GIT_SSH_COMMAND_PREFIX)
+        && command.ends_with(MANAGED_GIT_SSH_COMMAND_SUFFIX)
 }
 
 fn apply_proxy_env_overrides(
@@ -455,7 +509,10 @@ fn apply_proxy_env_overrides(
     // Keep local/private targets direct so local IPC and metadata endpoints avoid the proxy.
     set_env_keys(env, NO_PROXY_ENV_KEYS, DEFAULT_NO_PROXY_VALUE);
 
-    env.insert("ELECTRON_GET_USE_PROXY".to_string(), "true".to_string());
+    env.insert(
+        ELECTRON_GET_USE_PROXY_ENV_KEY.to_string(),
+        "true".to_string(),
+    );
 
     // Keep HTTP_PROXY/HTTPS_PROXY as HTTP endpoints. A lot of clients break if
     // those vars contain SOCKS URLs. We only switch ALL_PROXY here.
@@ -471,9 +528,17 @@ fn apply_proxy_env_overrides(
     #[cfg(target_os = "macos")]
     if socks_enabled {
         // Preserve existing SSH wrappers (for example: Secretive/Teleport setups)
-        // and only provide a SOCKS ProxyCommand fallback when one is not present.
-        env.entry("GIT_SSH_COMMAND".to_string())
-            .or_insert_with(|| format!("ssh -o ProxyCommand='nc -X 5 -x {socks_addr} %h %p'"));
+        // but refresh a previously injected Codex fallback so it cannot point
+        // at a stale proxy port after the proxy is restarted.
+        match env.get(GIT_SSH_COMMAND_ENV_KEY) {
+            Some(command) if !is_managed_git_ssh_command(command) => {}
+            _ => {
+                env.insert(
+                    GIT_SSH_COMMAND_ENV_KEY.to_string(),
+                    managed_git_ssh_command(socks_addr),
+                );
+            }
+        }
     }
 }
 
@@ -928,14 +993,36 @@ mod tests {
             Some(&DEFAULT_NO_PROXY_VALUE.to_string())
         );
         assert_eq!(env.get(ALLOW_LOCAL_BINDING_ENV_KEY), Some(&"0".to_string()));
-        assert_eq!(env.get("ELECTRON_GET_USE_PROXY"), Some(&"true".to_string()));
+        assert_eq!(
+            env.get(ELECTRON_GET_USE_PROXY_ENV_KEY),
+            Some(&"true".to_string())
+        );
         #[cfg(target_os = "macos")]
         assert_eq!(
-            env.get("GIT_SSH_COMMAND"),
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
             Some(&"ssh -o ProxyCommand='nc -X 5 -x 127.0.0.1:8081 %h %p'".to_string())
         );
         #[cfg(not(target_os = "macos"))]
-        assert_eq!(env.get("GIT_SSH_COMMAND"), None);
+        assert_eq!(env.get(GIT_SSH_COMMAND_ENV_KEY), None);
+    }
+
+    #[test]
+    fn apply_proxy_env_overrides_sets_only_managed_proxy_env_keys() {
+        let mut env = HashMap::new();
+        apply_proxy_env_overrides(
+            &mut env,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 3128),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8081),
+            /*socks_enabled*/ true,
+            /*allow_local_binding*/ false,
+        );
+
+        for key in env.keys() {
+            assert!(
+                MANAGED_PROXY_ENV_KEYS.contains(&key.as_str()),
+                "proxy env writer set key missing from MANAGED_PROXY_ENV_KEYS: {key}"
+            );
+        }
     }
 
     #[test]
@@ -989,11 +1076,11 @@ mod tests {
         );
         #[cfg(target_os = "macos")]
         assert_eq!(
-            env.get("GIT_SSH_COMMAND"),
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
             Some(&"ssh -o ProxyCommand='nc -X 5 -x 127.0.0.1:8081 %h %p'".to_string())
         );
         #[cfg(not(target_os = "macos"))]
-        assert_eq!(env.get("GIT_SSH_COMMAND"), None);
+        assert_eq!(env.get(GIT_SSH_COMMAND_ENV_KEY), None);
     }
 
     #[cfg(target_os = "macos")]
@@ -1001,7 +1088,7 @@ mod tests {
     fn apply_proxy_env_overrides_preserves_existing_git_ssh_command() {
         let mut env = HashMap::new();
         env.insert(
-            "GIT_SSH_COMMAND".to_string(),
+            GIT_SSH_COMMAND_ENV_KEY.to_string(),
             "ssh -o ProxyCommand='tsh proxy ssh --cluster=dev %r@%h:%p'".to_string(),
         );
         apply_proxy_env_overrides(
@@ -1013,8 +1100,34 @@ mod tests {
         );
 
         assert_eq!(
-            env.get("GIT_SSH_COMMAND"),
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
             Some(&"ssh -o ProxyCommand='tsh proxy ssh --cluster=dev %r@%h:%p'".to_string())
+        );
+    }
+
+    #[cfg(target_os = "macos")]
+    #[test]
+    fn apply_proxy_env_overrides_refreshes_previous_managed_git_ssh_command() {
+        let mut env = HashMap::new();
+        env.insert(
+            GIT_SSH_COMMAND_ENV_KEY.to_string(),
+            managed_git_ssh_command(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8081)),
+        );
+
+        apply_proxy_env_overrides(
+            &mut env,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 43128),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 48081),
+            /*socks_enabled*/ true,
+            /*allow_local_binding*/ false,
+        );
+
+        assert_eq!(
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
+            Some(&managed_git_ssh_command(SocketAddr::new(
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                48081,
+            )))
         );
     }
 }

Yz613/codex-for-open-source

codex-rs/core/src/tools/runtimes/mod.rs
@@ -8,6 +8,8 @@ use crate::exec_env::CODEX_THREAD_ID_ENV_VAR;
 use crate::path_utils;
 use crate::shell::Shell;
 use crate::tools::sandboxing::ToolError;
+use codex_network_proxy::PROXY_ACTIVE_ENV_KEY;
+use codex_network_proxy::PROXY_ENV_KEYS;
 use codex_protocol::models::PermissionProfile;
 use codex_sandboxing::SandboxCommand;
 use codex_utils_absolute_path::AbsolutePathBuf;
@@ -102,6 +104,9 @@ pub(crate) fn maybe_wrap_shell_lc_with_snapshot(
         override_env.insert(CODEX_THREAD_ID_ENV_VAR.to_string(), thread_id.clone());
     }
     let (override_captures, override_exports) = build_override_exports(&override_env);
+    let (proxy_captures, proxy_exports) = build_proxy_env_exports();
+    let override_captures = join_shell_blocks([override_captures, proxy_captures]);
+    let override_exports = join_shell_blocks([override_exports, proxy_exports]);
     let rewritten_script = if override_exports.is_empty() {
         format!(
             "if . '{snapshot_path}' >/dev/null 2>&1; then :; fi\n\nexec '{original_shell}' -c '{original_script}'{trailing_args}"
@@ -118,10 +123,35 @@ pub(crate) fn maybe_wrap_shell_lc_with_snapshot(
 fn build_override_exports(explicit_env_overrides: &HashMap<String, String>) -> (String, String) {
     let mut keys = explicit_env_overrides
         .keys()
+        .map(String::as_str)
         .filter(|key| is_valid_shell_variable_name(key))
         .collect::<Vec<_>>();
     keys.sort_unstable();
 
+    build_override_exports_for_keys("__CODEX_SNAPSHOT_OVERRIDE", &keys)
+}
+
+fn build_proxy_env_exports() -> (String, String) {
+    let mut keys = PROXY_ENV_KEYS
+        .iter()
+        .copied()
+        .filter(|key| is_valid_shell_variable_name(key))
+        .collect::<Vec<_>>();
+    keys.sort_unstable();
+    keys.dedup();
+
+    let (captures, restores) =
+        build_override_exports_for_keys("__CODEX_SNAPSHOT_PROXY_OVERRIDE", &keys);
+    let key = PROXY_ACTIVE_ENV_KEY;
+    (
+        format!("{captures}\n__CODEX_SNAPSHOT_PROXY_ENV_SET=\"${{{key}+x}}\""),
+        format!(
+            "if [ -n \"$__CODEX_SNAPSHOT_PROXY_ENV_SET\" ] || [ -n \"${{{key}+x}}\" ]; then\n{restores}\nfi"
+        ),
+    )
+}
+
+fn build_override_exports_for_keys(variable_prefix: &str, keys: &[&str]) -> (String, String) {
     if keys.is_empty() {
         return (String::new(), String::new());
     }
@@ -130,9 +160,9 @@ fn build_override_exports(explicit_env_overrides: &HashMap<String, String>) -> (
         .iter()
         .enumerate()
         .map(|(idx, key)| {
-            format!(
-                "__CODEX_SNAPSHOT_OVERRIDE_SET_{idx}=\"${{{key}+x}}\"\n__CODEX_SNAPSHOT_OVERRIDE_{idx}=\"${{{key}-}}\""
-            )
+            let set_var = format!("{variable_prefix}_SET_{idx}");
+            let value_var = format!("{variable_prefix}_{idx}");
+            format!("{set_var}=\"${{{key}+x}}\"\n{value_var}=\"${{{key}-}}\"")
         })
         .collect::<Vec<_>>()
         .join("\n");
@@ -140,14 +170,24 @@ fn build_override_exports(explicit_env_overrides: &HashMap<String, String>) -> (
         .iter()
         .enumerate()
         .map(|(idx, key)| {
+            let set_var = format!("{variable_prefix}_SET_{idx}");
+            let value_var = format!("{variable_prefix}_{idx}");
             format!(
-                "if [ -n \"${{__CODEX_SNAPSHOT_OVERRIDE_SET_{idx}}}\" ]; then export {key}=\"${{__CODEX_SNAPSHOT_OVERRIDE_{idx}}}\"; else unset {key}; fi"
+                "if [ -n \"${{{set_var}}}\" ]; then export {key}=\"${{{value_var}}}\"; else unset {key}; fi"
             )
         })
         .collect::<Vec<_>>()
         .join("\n");
 
     (captures, restores)
+}
+
+fn join_shell_blocks(blocks: impl IntoIterator<Item = String>) -> String {
+    blocks
+        .into_iter()
+        .filter(|block| !block.is_empty())
+        .collect::<Vec<_>>()
+        .join("\n")
 }
 
 fn is_valid_shell_variable_name(name: &str) -> bool {

codex-rs/network-proxy/src/lib.rs
@@ -38,6 +38,8 @@ pub use proxy::NO_PROXY_ENV_KEYS;
 pub use proxy::NetworkProxy;
 pub use proxy::NetworkProxyBuilder;
 pub use proxy::NetworkProxyHandle;
+pub use proxy::PROXY_ACTIVE_ENV_KEY;
+pub use proxy::PROXY_ENV_KEYS;
 pub use proxy::PROXY_URL_ENV_KEYS;
 pub use proxy::has_proxy_url_env_vars;
 pub use proxy::proxy_url_env_value;

codex-rs/network-proxy/src/proxy.rs
@@ -363,7 +363,47 @@ pub const PROXY_URL_ENV_KEYS: &[&str] = &[
 ];
 
 pub const ALL_PROXY_ENV_KEYS: &[&str] = &["ALL_PROXY", "all_proxy"];
+pub const PROXY_ACTIVE_ENV_KEY: &str = "CODEX_NETWORK_PROXY_ACTIVE";
 pub const ALLOW_LOCAL_BINDING_ENV_KEY: &str = "CODEX_NETWORK_ALLOW_LOCAL_BINDING";
+const ELECTRON_GET_USE_PROXY_ENV_KEY: &str = "ELECTRON_GET_USE_PROXY";
+const GIT_SSH_COMMAND_ENV_KEY: &str = "GIT_SSH_COMMAND";
+pub const PROXY_ENV_KEYS: &[&str] = &[
+    PROXY_ACTIVE_ENV_KEY,
+    ALLOW_LOCAL_BINDING_ENV_KEY,
+    ELECTRON_GET_USE_PROXY_ENV_KEY,
+    GIT_SSH_COMMAND_ENV_KEY,
+    "HTTP_PROXY",
+    "HTTPS_PROXY",
+    "http_proxy",
+    "https_proxy",
+    "YARN_HTTP_PROXY",
+    "YARN_HTTPS_PROXY",
+    "npm_config_http_proxy",
+    "npm_config_https_proxy",
+    "npm_config_proxy",
+    "NPM_CONFIG_HTTP_PROXY",
+    "NPM_CONFIG_HTTPS_PROXY",
+    "NPM_CONFIG_PROXY",
+    "BUNDLE_HTTP_PROXY",
+    "BUNDLE_HTTPS_PROXY",
+    "PIP_PROXY",
+    "DOCKER_HTTP_PROXY",
+    "DOCKER_HTTPS_PROXY",
+    "WS_PROXY",
+    "WSS_PROXY",
+    "ws_proxy",
+    "wss_proxy",
+    "NO_PROXY",
+    "no_proxy",
+    "npm_config_noproxy",
+    "NPM_CONFIG_NOPROXY",
+    "YARN_NO_PROXY",
+    "BUNDLE_NO_PROXY",
+    "ALL_PROXY",
+    "all_proxy",
+    "FTP_PROXY",
+    "ftp_proxy",
+];
 
 const FTP_PROXY_ENV_KEYS: &[&str] = &["FTP_PROXY", "ftp_proxy"];
 const WEBSOCKET_PROXY_ENV_KEYS: &[&str] = &["WS_PROXY", "WSS_PROXY", "ws_proxy", "wss_proxy"];
@@ -384,6 +424,11 @@ pub const DEFAULT_NO_PROXY_VALUE: &str = concat!(
     "172.16.0.0/12,",
     "192.168.0.0/16"
 );
+
+#[cfg(target_os = "macos")]
+const MANAGED_GIT_SSH_COMMAND_PREFIX: &str = "ssh -o ProxyCommand='nc -X 5 -x ";
+#[cfg(target_os = "macos")]
+const MANAGED_GIT_SSH_COMMAND_SUFFIX: &str = " %h %p'";
 
 pub fn proxy_url_env_value<'a>(
     env: &'a HashMap<String, String>,
@@ -408,6 +453,17 @@ fn set_env_keys(env: &mut HashMap<String, String>, keys: &[&str], value: &str) {
     }
 }
 
+#[cfg(target_os = "macos")]
+fn managed_git_ssh_command(socks_addr: SocketAddr) -> String {
+    format!("{MANAGED_GIT_SSH_COMMAND_PREFIX}{socks_addr}{MANAGED_GIT_SSH_COMMAND_SUFFIX}")
+}
+
+#[cfg(target_os = "macos")]
+fn is_managed_git_ssh_command(command: &str) -> bool {
+    command.starts_with(MANAGED_GIT_SSH_COMMAND_PREFIX)
+        && command.ends_with(MANAGED_GIT_SSH_COMMAND_SUFFIX)
+}
+
 fn apply_proxy_env_overrides(
     env: &mut HashMap<String, String>,
     http_addr: SocketAddr,
@@ -417,6 +473,7 @@ fn apply_proxy_env_overrides(
 ) {
     let http_proxy_url = format!("http://{http_addr}");
     let socks_proxy_url = format!("socks5h://{socks_addr}");
+    env.insert(PROXY_ACTIVE_ENV_KEY.to_string(), "1".to_string());
     env.insert(
         ALLOW_LOCAL_BINDING_ENV_KEY.to_string(),
         if allow_local_binding {
@@ -459,7 +516,10 @@ fn apply_proxy_env_overrides(
     // locally instead of letting the proxy resolve them.
     set_env_keys(env, NO_PROXY_ENV_KEYS, DEFAULT_NO_PROXY_VALUE);
 
-    env.insert("ELECTRON_GET_USE_PROXY".to_string(), "true".to_string());
+    env.insert(
+        ELECTRON_GET_USE_PROXY_ENV_KEY.to_string(),
+        "true".to_string(),
+    );
 
     // Keep HTTP_PROXY/HTTPS_PROXY as HTTP endpoints. A lot of clients break if
     // those vars contain SOCKS URLs. We only switch ALL_PROXY here.
@@ -475,9 +535,17 @@ fn apply_proxy_env_overrides(
     #[cfg(target_os = "macos")]
     if socks_enabled {
         // Preserve existing SSH wrappers (for example: Secretive/Teleport setups)
-        // and only provide a SOCKS ProxyCommand fallback when one is not present.
-        env.entry("GIT_SSH_COMMAND".to_string())
-            .or_insert_with(|| format!("ssh -o ProxyCommand='nc -X 5 -x {socks_addr} %h %p'"));
+        // but refresh a previously injected Codex fallback so it cannot point
+        // at a stale proxy port after the proxy is restarted.
+        match env.get(GIT_SSH_COMMAND_ENV_KEY) {
+            Some(command) if !is_managed_git_ssh_command(command) => {}
+            _ => {
+                env.insert(
+                    GIT_SSH_COMMAND_ENV_KEY.to_string(),
+                    managed_git_ssh_command(socks_addr),
+                );
+            }
+        }
     }
 }
 
@@ -936,15 +1004,38 @@ mod tests {
         assert!(no_proxy.contains("172.16.0.0/12"));
         assert!(no_proxy.contains("192.168.0.0/16"));
         assert!(no_proxy.contains("169.254.0.0/16"));
+        assert_eq!(env.get(PROXY_ACTIVE_ENV_KEY), Some(&"1".to_string()));
         assert_eq!(env.get(ALLOW_LOCAL_BINDING_ENV_KEY), Some(&"0".to_string()));
-        assert_eq!(env.get("ELECTRON_GET_USE_PROXY"), Some(&"true".to_string()));
+        assert_eq!(
+            env.get(ELECTRON_GET_USE_PROXY_ENV_KEY),
+            Some(&"true".to_string())
+        );
         #[cfg(target_os = "macos")]
         assert_eq!(
-            env.get("GIT_SSH_COMMAND"),
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
             Some(&"ssh -o ProxyCommand='nc -X 5 -x 127.0.0.1:8081 %h %p'".to_string())
         );
         #[cfg(not(target_os = "macos"))]
-        assert_eq!(env.get("GIT_SSH_COMMAND"), None);
+        assert_eq!(env.get(GIT_SSH_COMMAND_ENV_KEY), None);
+    }
+
+    #[test]
+    fn apply_proxy_env_overrides_sets_only_proxy_env_keys() {
+        let mut env = HashMap::new();
+        apply_proxy_env_overrides(
+            &mut env,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 3128),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8081),
+            /*socks_enabled*/ true,
+            /*allow_local_binding*/ false,
+        );
+
+        for key in env.keys() {
+            assert!(
+                PROXY_ENV_KEYS.contains(&key.as_str()),
+                "proxy env writer set key missing from PROXY_ENV_KEYS: {key}"
+            );
+        }
     }
 
     #[test]
@@ -998,11 +1089,11 @@ mod tests {
         );
         #[cfg(target_os = "macos")]
         assert_eq!(
-            env.get("GIT_SSH_COMMAND"),
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
             Some(&"ssh -o ProxyCommand='nc -X 5 -x 127.0.0.1:8081 %h %p'".to_string())
         );
         #[cfg(not(target_os = "macos"))]
-        assert_eq!(env.get("GIT_SSH_COMMAND"), None);
+        assert_eq!(env.get(GIT_SSH_COMMAND_ENV_KEY), None);
     }
 
     #[cfg(target_os = "macos")]
@@ -1010,7 +1101,7 @@ mod tests {
     fn apply_proxy_env_overrides_preserves_existing_git_ssh_command() {
         let mut env = HashMap::new();
         env.insert(
-            "GIT_SSH_COMMAND".to_string(),
+            GIT_SSH_COMMAND_ENV_KEY.to_string(),
             "ssh -o ProxyCommand='tsh proxy ssh --cluster=dev %r@%h:%p'".to_string(),
         );
         apply_proxy_env_overrides(
@@ -1022,8 +1113,34 @@ mod tests {
         );
 
         assert_eq!(
-            env.get("GIT_SSH_COMMAND"),
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
             Some(&"ssh -o ProxyCommand='tsh proxy ssh --cluster=dev %r@%h:%p'".to_string())
+        );
+    }
+
+    #[cfg(target_os = "macos")]
+    #[test]
+    fn apply_proxy_env_overrides_refreshes_previous_managed_git_ssh_command() {
+        let mut env = HashMap::new();
+        env.insert(
+            GIT_SSH_COMMAND_ENV_KEY.to_string(),
+            managed_git_ssh_command(SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 8081)),
+        );
+
+        apply_proxy_env_overrides(
+            &mut env,
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 43128),
+            SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 48081),
+            /*socks_enabled*/ true,
+            /*allow_local_binding*/ false,
+        );
+
+        assert_eq!(
+            env.get(GIT_SSH_COMMAND_ENV_KEY),
+            Some(&managed_git_ssh_command(SocketAddr::new(
+                IpAddr::V4(Ipv4Addr::LOCALHOST),
+                48081,
+            )))
         );
     }
 }